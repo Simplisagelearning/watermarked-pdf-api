3b48d96d3f6abd8f1ebfa6d9fb90577c482ac9c0
Simplisagelearning <simplisage@gmail.com>
2025-05-22 21:48:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def watermark():
     return send_file(output_pdf, as_attachment=True)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=10000)
-
+   import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
